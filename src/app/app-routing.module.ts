0a913d7022665aa820198209be452f956d857935
rodriguezea1 <rodriguezea1@appstate.edu>
2025-04-23 15:53:30 -0400

    working login with different pages for each user type

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from '../app/loginStuff/admin/admin.component';
+import { ParentComponent } from '../app/loginStuff/parent/parent.component';
+import { StudentComponent } from '../app/loginStuff/student/student.component';
+import { LoginComponent } from '../app/loginStuff/login/login.component';
+import { NotFoundComponent } from '../app/loginStuff/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AdminComponent }, // Set to a default component (e.g., AdminComponent or Dashboard)
+  { path: 'admin', component: AdminComponent },
+  { path: 'parent', component: ParentComponent },
+  { path: 'student', component: StudentComponent },
+  { path: 'login', component: LoginComponent }, // Separate route for login
+  { path: '**', component: NotFoundComponent } // Catch-all for unknown routes
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,35 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './loginStuff/admin/admin.component';
+import { ParentComponent } from './loginStuff/parent/parent.component';
+import { StudentComponent } from './loginStuff/student/student.component';
+import { LoginComponent } from './loginStuff/login/login.component';
+import { NotFoundComponent } from './loginStuff/not-found/not-found.component';
+import { LogoutComponent } from './loginStuff/logout/logout.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    ParentComponent,
+    StudentComponent,
+    LoginComponent,
+    NotFoundComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
